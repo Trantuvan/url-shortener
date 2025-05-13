d8701e0392ec32a727b041438ea10e9281668ce5
trantuvan <trantuvan.kan@gmail.com>
2025-05-13 14:16:30 +0700

    batman

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/trantuvan/url-shortener
+
+go 1.24.3
---
+++ handlers/handler.go
@@ -0,0 +1,49 @@
+package handlers
+
+import (
+	"net/http"
+	"strings"
+)
+
+// MapHandler will return an http.HandlerFunc (which also
+// implements http.Handler) that will attempt to map any
+// paths (keys in the map) to their corresponding URL (values
+// that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback
+// http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	//	TODO: Implement this...
+	return func(w http.ResponseWriter, r *http.Request) {
+		for p := range strings.SplitSeq(r.URL.Path, "/") {
+			if len(p) <= 0 {
+				continue
+			}
+			if url, ok := pathsToUrls["/"+p]; ok {
+				http.Redirect(w, r, url, http.StatusMovedPermanently)
+				return
+			}
+		}
+		fallback.ServeHTTP(w, r)
+	}
+}
+
+// YAMLHandler will parse the provided YAML and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+//
+// YAML is expected to be in the format:
+//
+//   - path: /some-path
+//     url: https://www.some-url.com/demo
+//
+// The only errors that can be returned all related to having
+// invalid YAML data.
+//
+// See MapHandler to create a similar http.HandlerFunc via
+// a mapping of paths to urls.
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	// TODO: Implement this...
+	return nil, nil
+}
